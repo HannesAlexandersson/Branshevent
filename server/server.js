dc8dc447929802186a3f09c134b63a224467e707
Karolina <karolina.limanowska@gmail.com>
2024-03-28 01:55:51 +0100

    creating a prototype of company endpoint

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
 import express from 'express';
-import sqlite3 from 'sqlite3';
-import companyRoutes from './company.js'
+import companyRoutes from './company.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const server = express();
 const port = 3000;
+
+//cross origin resource sharing settings to make the API work
+const corsOptions = {
+    origin: ['*'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  };
+  
+server.use(cors(corsOptions));
+server.use(express.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json())
 
 server.use('/company/', companyRoutes);
 
