ac9d9dc21e0a8abd801d2533d55e1ed2c578c742
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-03-29 22:46:33 +0100

    Fixed dynamic site title on navbar
    added logic to navbar to acces current uri and set site title based on current uri

--- client/src/views/Navigation/Navigation.jsx
+++ client/src/views/Navigation/Navigation.jsx
@@ -1,21 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Outlet } from 'react-router-dom';
 import { home, event, account, yrgo } from '../../assets/Icons/dropdownicons/index.js';
 import styles from './navigation.module.css';
 import Ylogo from '../../assets/Logos/yrgo-logoRed.svg';
 
-
+const LocationContext = React.createContext();
 function Navigation(){
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  //dropdown logic:
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);    
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+    const toggleDropdown = () => {
+      setIsDropdownOpen(!isDropdownOpen);
+    };
+
+  //page title logic:
+    const [pathname, setPathname] = useState('');
+    const [pageTitle, setPageTitle] = useState('');
+    const navigate = useNavigate();
+
+
+    // array of all our route paths converted to page titles
+    const titleMap = {
+      '/': 'Home',
+      '/sign-in': 'SIGN IN',
+      '/company-signup': 'COMPANY SIGN-UP',
+    }    
+
+    useEffect(() => {
+      setPathname(window.location.pathname);
+      setPageTitle(titleMap[window.location.pathname] || 'Default Title');
+  }, []);
+
+    const formatPathname = (pathname) => {
+      return pathname.replace(/^\/+|\/+$/g, '');
   };
-
     return(
+      <LocationContext.Provider value={{ pathname: formatPathname(pathname), pageTitle }}>
         <div className={styles.main_nav}>
             <button onClick={toggleDropdown} className={styles.hamburger}>&#9776;</button>
             <div className={styles.title_container}>
-                <p className={styles.nav_title}>SIGN IN</p>
+                <p className={styles.nav_title}>{pageTitle}</p>
             </div>
             <img src={Ylogo} alt="yrgo logo small" />
             {isDropdownOpen && (
@@ -29,7 +53,13 @@ function Navigation(){
         </div>
       )}
         </div>
+        <Outlet />
+    </LocationContext.Provider>
     );
+}
+
+function useLocationInfo() {
+  return React.useContext(LocationContext);
 }
 
 export default Navigation
