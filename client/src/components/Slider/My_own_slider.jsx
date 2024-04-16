9d2434cde03e463b6d3ce93addd17c80fee6d057
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-04-16 08:18:32 +0200

    fixing the avatars for the slider

--- client/src/components/Slider/My_own_slider.jsx
+++ client/src/components/Slider/My_own_slider.jsx
@@ -1,29 +1,75 @@
-import React, { useState } from 'react';
-import props from 'prop-types';
-import style from './slider.module.css'; // Make sure to import your CSS file
+import React, { useState, useEffect } from 'react';
+import { briefcase, locationBlack, userSml } from '../../assets/Icons/index.js';
+import { heartlight } from '../../assets/Icons/dropdownicons/index.js';
+import style from './slider.module.css';
+import Get_avatars from '../../components/get_student_avatar/Get_avatars.jsx';
+import * as avatarsc from '../../assets/company_default_avatars/index';
+import Slide_show from './Slide_show.jsx';
 
-function MyOwnSlider({ companies, children }) {
-  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const goToNextSlide = () => {
-    const nextIndex = currentIndex === companies.length - 1 ? 0 : currentIndex + 1;
-    setCurrentIndex(nextIndex);
-  };
-
-  const goToPrevSlide = () => {
-    const prevIndex = currentIndex === 0 ? companies.length - 1 : currentIndex - 1;
-    setCurrentIndex(prevIndex);
-  };
-
-  return (
-    <div className={style.slider_container_child}>
-      {children[currentIndex]}
-      <div className={style.prev_nxt_cont}>
-        <button className={style.nxt_prev} onClick={goToPrevSlide}>&lt;-</button>
-        <button className={style.nxt_prev} onClick={goToNextSlide}>-&gt;</button>
-      </div>
-    </div>
-  );
-}
-
-export default MyOwnSlider;
+  
+  function MyOwnSlider({ companies, handleViewCompany }) {
+    const [imgMap, setImgMap] = useState({});
+  
+    useEffect(() => {
+      const fetchAvatars = async () => {
+        const token = localStorage.getItem('token');
+        const imgUrls = {};
+        
+        for (const company of companies) {
+          if (company.avatar_id) {
+            try {
+              const avatarData = await Get_avatars(company.id, token, 'companyAvatars/');
+              imgUrls[company.id] = URL.createObjectURL(avatarData);
+            } catch (error) {
+              console.error('Error fetching company data:', error);
+            }
+          } else {
+            const companyAvatars = Object.values(avatarsc);
+            const randomIndex = Math.floor(Math.random() * companyAvatars.length);
+            imgUrls[company.id] = companyAvatars[randomIndex];
+          }
+        }
+        
+        setImgMap(imgUrls);
+      };
+  
+      fetchAvatars();
+    }, [companies]);
+    {/* <div className={style.slider_container_child}> */}
+    return (
+      <Slide_show companies={companies}  >
+        {companies.map((company) => (
+          <div key={company.id} className={style.redBox} onClick={() => handleViewCompany(company.id)}>
+            <div className={style.img_wrapper}>
+              <div className={style.img_display_area}>
+                {/* Render avatar image or default avatar image */}
+                <img src={imgMap[company.id]} alt="company image / default avatar image" />
+                <div className={style.icon_container}>
+                  <img src={heartlight} alt="heart icon" />
+                </div>
+              </div>
+            </div>
+            <div className={style.text_area}>
+              <div className={style.name_box}>
+                <img src={briefcase} alt="briefcase icon" />
+                <p>{company.company_name}</p>
+              </div>
+              <div className={style.name_box}>
+                <div className={style.name_loc_wrapper}>
+                  <img src={userSml} alt="user icon" />
+                  <p>{company.first_name} {company.last_name}</p>
+                </div>
+                <div className={style.name_loc_wrapper}>                                
+                  <img src={locationBlack} alt="location icon" />
+                  <p>{company.location}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slide_show>
+    );
+  }
+  /* </div> */
+export default MyOwnSlider
--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -1,84 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate,  } from "react-router-dom";
-import * as avatarsc from '../../assets/company_default_avatars/index';
-import props from 'prop-types';
-import My_own_slider from "./My_own_slider.jsx";
-import { briefcase, locationBlack, userSml } from '../../assets/Icons/index.js';
-import { heartlight } from '../../assets/Icons/dropdownicons/index.js';
-import style from './slider.module.css';
-import Get_avatars from '../../components/get_student_avatar/Get_avatars.jsx';
 
+import My_own_slider from "./My_own_slider.jsx";
 
 function SimpleSlider({ companies }) {
-const navigate = useNavigate();
-const [avatarDataMap, setAvatarDataMap] = useState({});  // Store avatar data for each company
-const [avatarLoaded, setAvatarLoaded] = useState(false);
-const [imgMap, setImgMap] = useState({});  // Store image URLs for each company
+const navigate = useNavigate();  
 
-
-
-const token = localStorage.getItem('token');
-
-useEffect(() => {
-  // Convert avatar data to image URLs
-  if (avatarLoaded) {
-    const imgUrls = {};
-    Object.entries(avatarDataMap).forEach(([compId, avatarData]) => {
-      if (avatarData instanceof Blob) {        
-        // avatarData is a blob URL so we need
-        imgUrls[compId] = URL.createObjectURL(avatarData);
-      }
-    });
-
-    // Handle default avatars separately
-    const companyAvatars = Object.values(avatarsc);
-    companies.forEach(company => {
-      const compId = company.id;
-      if (!imgUrls[compId]) {
-        const randomIndex = Math.floor(Math.random() * companyAvatars.length);
-        imgUrls[compId] = companyAvatars[randomIndex];
-      }
-    });
-
-    setImgMap(imgUrls);
-  }
-}, [avatarDataMap, avatarLoaded, companies]);
-
-useEffect(() => {
-  // Convert avatar data to image URLs
-  if (avatarLoaded) {
-    const imgUrls = {};
-    console.log(avatarDataMap)
-    Object.entries(avatarDataMap).forEach(([compId, avatarData]) => {
-      if (avatarDataMap instanceof Blob) {        
-        // avatarData is a blob URL so we need
-        imgUrls[compId] = URL.createObjectURL(avatarData);
-        
-      } else {
-        // No avatar data, set default image URL
-        const companyAvatars = Object.values(avatarsc);
-        console.log('hejsan')
-        const randomIndex = Math.floor(Math.random() * companyAvatars.length);
-        imgUrls[compId] = companyAvatars[randomIndex];
-        
-        
-      }
-    });
-    setImgMap(imgUrls);
-  }
-}, [avatarDataMap, avatarLoaded]);
 
 const handleViewCompany = (companyId) => {
   navigate('/view-company', { state: { companyId, companies } });
 };
 
 return (
-  <My_own_slider companies={companies}>
-    {companies.map((company) => (
+  
+        <My_own_slider    
+          companies={companies}
+          onClick={handleViewCompany}
+        >
+        
+            
+          
+        </My_own_slider>
+ 
+);
+/*
+ {companies.map((company) => (
       <div key={company.id} className={style.redBox} onClick={() => handleViewCompany(company.id)}>
         <div className={style.img_wrapper}>
           <div className={style.img_display_area}>
-            {/* Render avatar image or default avatar image */}
+       
             <img src={imgMap[company.id]} alt="company image / default avatar image" />
             <div className={style.icon_container}>
               <img src={heartlight} alt="heart icon" />
@@ -102,9 +52,11 @@ return (
           </div>
         </div>
       </div>
-    ))}
-  </My_own_slider>
-);
+    
+
+*/
+
+
 }
 
 
