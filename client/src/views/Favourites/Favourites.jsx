2bc694e01eaec68d1b8fd0ee323415345767c802
Karolina <karolina.limanowska@gmail.com>
2024-04-16 13:56:31 +0200

    removing search bar from the favorite view for the company

--- client/src/views/Favourites/Favourites.jsx
+++ client/src/views/Favourites/Favourites.jsx
@@ -84,51 +84,9 @@ function Favourites(){
                 </div>
                 
 
-                
+                 <div className={style.attending_comps}>
 
-               
-
-                <div className={style.attending_comps}>
-
-                <div className={style.search_wrapper_large}>
-                    <div className={style.searchbar}>
-                        <input 
-                        className={style.searchbar_input} 
-                        type="text" 
-                        id="search-input" 
-                        placeholder="Search companies" />
-                        <img src={search} />
-                    </div>
-                </div>
-
-
-                <div className={style.filter_wrapper}>
-                    <div className={`${style.filter_btn} ${showFilter ? style.active : ''}`}>
-                        <div className={style.filter_btn_content}>
-                            <p className={style.filt}>FILTER</p>
-                            <button onClick={handleFilter} className={style.filter_btn_btn}>
-                                <img src={sliders} alt="sliders" />
-                            </button>
-                        </div>
-                        {showFilter && (
-                        <div className={style.dropdowns_wrapper}>
-                            {/* Dropdown for tags */}
-                            <select className={style.dropdown}>
-                            <option value="" disabled selected hidden>Tags</option>
-                            </select>
-                            {/* Dropdown for workplace */}
-                            <select className={style.dropdown}>
-                                <option value="" disabled selected hidden>Workplace</option>
-                                <option value="office">Office</option>
-                                <option value="remote">Remote</option>
-                                <option value="both">Both</option>
-                            </select>
-                        </div>
-                        )}
-                    </div>
-                </div>
-
-
+            
                     <p>All Favourites</p>
 
                     <div className={style.mini_cards_containter}>
