0f2938511012954fc17bc5c022c8c8fdc5387071
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-04-17 07:10:11 +0200

    urls

--- client/src/components/get_student_avatar/Get_avatars.jsx
+++ client/src/components/get_student_avatar/Get_avatars.jsx
@@ -1,6 +1,6 @@
 // company/companyAvatars, student/studentAvatars
 function Get_avatars( id, token, endpoint){    
-    const baseUrl = 'http://localhost:3000/api/api/avatars/';
+    const baseUrl = 'http://localhost:3000/api/avatars/';
     const providedEndpoint = endpoint;
     const userId = id;
     const url = baseUrl + providedEndpoint + userId;
