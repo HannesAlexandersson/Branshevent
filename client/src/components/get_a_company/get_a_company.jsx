317b825101782461a0c34b734871b317467314fa
Hannes Alexandersson <S103154@gmail.com>
2024-04-17 07:29:39 +0200

    dd

--- client/src/components/get_a_company/get_a_company.jsx
+++ client/src/components/get_a_company/get_a_company.jsx
@@ -2,7 +2,7 @@
 
 function get_a_company(token, id){
     const companyId = id;
-    const baseUrl = 'http://localhost:3000/api/';
+    const baseUrl = 'http://localhost:3000/api/company/';
     const url = baseUrl + companyId;
 
     return fetch(url, { 
